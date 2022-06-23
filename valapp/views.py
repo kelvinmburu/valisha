d349c93636896bac558b9cefba57c976f03314ee
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-23 20:55:25 +0300

    feat: run migrations

--- valapp/views.py
+++ valapp/views.py
@@ -1,6 +1,63 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import RegisterForm
+from .emails import send_welcome_email
+from valapp.models import Profile
 
 # Create your views here.
+
+def login_user(request):
+    page = 'login'
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+    ctx = {'page': page}
+    return render(request, 'valapp/auth.html', ctx)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
+def register_user(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            recipient = User(username=username, email=email)
+            send_welcome_email(username, email)
+
+            profile = Profile.objects.create(owner=user)
+            profile.save()
+            
+            page = 'register'
+            return render(request, 'valapp/success.html',{'page': page})
+    ctx = {'form': form}
+    return render(request, 'valapp/auth.html', ctx)
+
 def home(request):
     context = {}
     return render(request, 'valapp/index.html', context)
@@ -24,3 +81,4 @@ def blog(request):
 def contact(request):
     context = {}
     return render(request, 'valapp/contact.html', context)
+
--- valprojec/settings.py
+++ valprojec/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'valapp'
+    'valapp',
+    'pyuploadcare.dj',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -129,7 +132,24 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+# Pyuploadcare
+UPLOADCARE = {
+    'pub_key': '7d016febe1ddd01a0314',
+    'secret': '675b39973c176d900497',
+}
+
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
