71a1aefc4043bc2d58d67d26ae4dc38d94e2a200
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-24 00:36:31 +0300

    feat: updated profiles model

--- valapp/models.py
+++ valapp/models.py
@@ -27,10 +27,33 @@ class Center(models.Model):
 class Profile(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=300, blank=True, null=True)
-    pic = models.ImageField(upload_to='images/', blank = True)
+    pic = models.ImageField(upload_to='images/', default='default.jpg', blank = True)
     center = models.ForeignKey(Center, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.owner.username
+    
+    
+class Cloth(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300, blank=True, null=True)
+    location = models.CharField(max_length=300)
+    contact = models.CharField(max_length=300, blank=True, null=True)
+    hood = models.ForeignKey(Center, on_delete=models.SET_NULL, null=True, blank=True)
+    image = ImageField(manual_crop='1280x720')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) :
+        return self.name
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
     
 
