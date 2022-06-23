9a9c08f3345e6cd3b9bb983997f35089d041d661
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-23 20:54:18 +0300

    feat: added forms for signin/up

---
+++ valapp/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+# Create your forms here
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, label='',widget=forms.EmailInput(attrs={'class': 'form-control mb-4', 'placeholder': 'Email'}))
+    username =forms.CharField(max_length=200, label='',widget=forms.TextInput(attrs={'class': 'form-control mb-4','placeholder': 'Username'}))
+    password1 = forms.CharField(max_length=200,label='',widget=forms.PasswordInput(attrs={'class': 'form-control mb-4', 'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=200, label='',widget=forms.PasswordInput(attrs={'class': 'form-control mb-4','placeholder': 'Confirm password'}))
+    
+    class Meta():
+       model=User
+       fields = ['email', 'username', 'password1', 'password2']
