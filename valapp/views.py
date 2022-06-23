4f501b407bb79c499dfca790f2a08f50fa5307f7
kelvinmburu <mburuhkelvin@gmail.com>
2022-06-24 00:36:52 +0300

    feat: updated profile update paths and func

--- valapp/views.py
+++ valapp/views.py
@@ -6,9 +6,21 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterForm
 from .emails import send_welcome_email
 from valapp.models import Profile
+from .forms import UpdateProfileForm
+
+from django.conf import settings
+from mailchimp_marketing import Client
+from mailchimp_marketing.api_client import ApiClientError
+
+
+# Mailchimp Settings
+api_key = settings.MAILCHIMP_API_KEY
+server = settings.MAILCHIMP_DATA_CENTER
+list_id = settings.MAILCHIMP_EMAIL_LIST_ID
 
 # Create your views here.
 
+# User login function
 def login_user(request):
     page = 'login'
     if request.method == 'POST':
@@ -30,11 +42,12 @@ def login_user(request):
     ctx = {'page': page}
     return render(request, 'valapp/auth.html', ctx)
 
+# User logout function
 def logout_user(request):
     logout(request)
     return redirect('home')
 
-
+# User register function
 def register_user(request):
     form = RegisterForm()
 
@@ -58,6 +71,7 @@ def register_user(request):
     ctx = {'form': form}
     return render(request, 'valapp/auth.html', ctx)
 
+# Basic page navigation settings
 def home(request):
     context = {}
     return render(request, 'valapp/index.html', context)
@@ -82,3 +96,55 @@ def contact(request):
     context = {}
     return render(request, 'valapp/contact.html', context)
 
+# Profile section
+def user_profile(request):
+    profile = Profile.objects.get(owner=request.user)
+    ctx = {'profile': profile,}
+    return render(request, 'valapp/profile.html', ctx)
+
+def update_profile(request):
+    profile = Profile.objects.get(owner=request.user)
+    form = UpdateProfileForm(instance=profile)
+    if request.method == 'POST':
+        form = UpdateProfileForm(
+            request.POST, request.FILES, instance=profile )
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+
+    ctx = {'profile': profile, 'form': form}
+    return render(request, 'valapp/update-profile.html', ctx)
+
+# Subscription Logic
+def subscribe(email):
+    """
+     Contains code handling the communication to the mailchimp api
+     to create a contact/member in an audience/list.
+    """
+
+    mailchimp = Client()
+    mailchimp.set_config({
+        "api_key": api_key,
+        "server": server,
+    })
+
+    member_info = {
+        "email_address": email,
+        "status": "subscribed",
+    }
+
+    try:
+        response = mailchimp.lists.add_list_member(list_id, member_info)
+        print("response: {}".format(response))
+    except ApiClientError as error:
+        print("An exception occurred: {}".format(error.text))
+
+# Mailchimp function
+
+def subscription(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        subscribe(email)
+        messages.success(request, "Email received. thank You! ") # message
+
+    return render(request, 'valapp/index.html')
